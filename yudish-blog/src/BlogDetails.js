d7fe6bc77e0f3eb0b65ba5a764c0f006ff26d7e3
shubhamxthakur <stfushubham@gmail.com>
2023-09-10 23:59:27 +0530

    Create Component added

--- yudish-blog/src/BlogDetails.js
+++ yudish-blog/src/BlogDetails.js
@@ -13,19 +13,19 @@ const BlogDetails = () => {
             history.push('/');
         })
     }
-}
 
-return (
-    <div className = "blog-details">
-        {isPending && <div>Loading...</div>}
-        {error && <div>{error}</div>}
-        <article>
-            <h2>{blog.title}</h2>
-            <p>Written by {blog.author}</p>
-            <div>{blog.body}</div>
-            <button onClick = {handleClick}>Delete</button>
-        </article>
-    </div>
-);
+    return (
+        <div className = "blog-details">
+            {isPending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            <article>
+                <h2>{blog.title}</h2>
+                <p>Written by {blog.author}</p>
+                <div>{blog.body}</div>
+                <button onClick = {handleClick}>Delete</button>
+            </article>
+        </div>
+    );
+}
 
 export default BlogDetails;
