b6cb52b194a037cb2a6e3b3f2f4c04ba8538c01a
shubhamxthakur <stfushubham@gmail.com>
2023-09-11 01:21:03 +0530

    useFetch created

---
+++ yudish-blog/src/useFetch.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+
+const useFetch = (url) => {
+    const [data, setData] = useState([]);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const abosrtCont = new AbortController();
+
+        setTimeout(() => {
+            fetch(url, { signal: AbortController.signal }).then(res => {
+                if(!res.ok){
+                    throw Error('Could not fetch data for that resource');
+                }
+                return res.json();
+            }).then((data) => {
+                setData(data);
+                setIsPending(false);
+                setError(null);
+            })
+            .catch((err) => {
+                if(err.name === 'AbortError') {
+                    console.log('Fetch Aborted');
+                }
+                else {
+                    setError(err.message);
+                    setIsPending(false);
+                }
+            })
+        }, 5);
+
+        return () => AbortController.abort();
+    }, [url]);
+
+    return {data, isPending, error};
+}
+
+export default useFetch;
